454c24a72d31fb117357ff1fb3baad92aa5c6fe6
Zhiyuan He <zh2312@columbia.edu>
2020-09-05 11:13:54 -0400

    Test numpy and demonstrate a layer

--- pl.py
+++ pl.py
@@ -1,15 +1,17 @@
 inputs = [1, 2, 3, 2.5]
 
-weights1= [0.2, 0.8, -0.5, 1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
+weights= [[0.2, 0.8, -0.5, 1.0],
+          [0.5, -0.91, 0.26, -0.5],
+          [-0.26, -0.27, 0.17, 0.87]]
 
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+biases = [2, 3, 0.5]
 
-output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + bias1,
-           inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + bias2,
-           inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + bias3]
+layer_outputs = []
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_ouput = 0
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_ouput += n_input*weight
+    neuron_bias += neuron_bias
+    layer_outputs.append(neuron_ouput)
 
-print(output)
+print(layer_outputs)
